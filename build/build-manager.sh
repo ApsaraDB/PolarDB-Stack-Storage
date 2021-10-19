4c0d424f9e2825c723155fd6e5c33c02dc50ad4d
chantu.lhl <chantu.lhl@alibaba-inc.com>
2021-10-19 20:47:01 +0800

    update build script

--- build/build-manager.sh
+++ build/build-manager.sh
@@ -1,4 +1,4 @@
-docker build -t reg.docker.alibaba-inc.com/polarbox/polardb-sms-manager:1.0.0 \
+docker build -t polardb/polardb-sms-manager:1.0.0 \
   --build-arg CodeSource=$(git remote -v | head -n 1 | awk '{print $2}') \
   --build-arg CodeBranch=$(git branch --show-current) \
   --build-arg CodeVersion=$(git rev-parse HEAD) \
@@ -6,4 +6,3 @@ docker build -t reg.docker.alibaba-inc.com/polarbox/polardb-sms-manager:1.0.0 \
   --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" \
   --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" \
   . -f build/manager/Dockerfile
-docker push reg.docker.alibaba-inc.com/polarbox/polardb-sms-manager:1.0.0
